4b8b8dcf5f61de1f49afbaf94c165a4c088d8147
jeffemart <jefferson.github@gmail.com>
2023-10-19 13:48:04 -0300

    Adicionando rotas de exemplo ao admin

--- myproject/myapi/admin.py
+++ myproject/myapi/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Chamado
 
-# Register your models here.
+admin.site.register(Chamado)
--- myproject/myapi/views.py
+++ myproject/myapi/views.py
@@ -1,8 +1,14 @@
 from rest_framework import viewsets
 from .models import Chamado
 from .serializers import ChamadoSerializer
+from drf_yasg.utils import swagger_auto_schema
 
 
 class ChamadoViewSet(viewsets.ModelViewSet):
     queryset = Chamado.objects.all()
     serializer_class = ChamadoSerializer
+
+    # exemplo de metodo para esconder algum REST que não queira
+    # @swagger_auto_schema(auto_schema=None)
+    # def partial_update(self, request, *args, **kwargs):
+    #     return super().partial_update(request, *args, **kwargs)
